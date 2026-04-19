105a216b0413228074f9c2cd27a322a40cfdfd0c
mtaufikbaskoro <mtaufikbaskoro@gmail.com>
2026-04-19 23:17:40 +0700

    init: add framer-motion to the projects

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,18 @@
+import { Button } from "@/components/ui/button";
 
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <h1>Hello World</h1>
+    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 dark:bg-black">
+      <main className="flex flex-1 w-full max-w-md flex-col items-center justify-evenly py-32 px-16 bg-white dark:bg-black sm:items-start">
+        <div className="text-center w-full space-y-2">
+          <h1 className="font-bold text-4xl">TODO TODO</h1>
+          <h3 className="font-light">An app for your weekly tasks</h3>
+        </div>
+        <div className="w-full space-y-2">
+          <Button className={"w-full"} variant="outline">Sign In</Button>
+          <Button className={"w-full"}>Sign Up</Button>
+        </div>
       </main>
     </div>
   );
